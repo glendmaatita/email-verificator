098fad9679bd489defde10a879e6c9b811ce2728
Glend Maatita <glend.maatita@gmail.com>
2018-01-04 07:01:34 +0700

    adding mailbox layer

--- src/providers/Mailboxlayer.php
+++ src/providers/Mailboxlayer.php
@@ -3,11 +3,26 @@
 namespace EmailVerificator\Providers;
 
 use EmailVerificator\ProviderInterface;
+use EmailVerificator\AbstractVerificator;
 
-class Mailboxlayer implements ProviderInterface
+class Mailboxlayer extends AbstractVerificator implements ProviderInterface
 {
     public function check()
     {
+        $baseUri = 'http://apilayer.net/api/';
 
+        if ($this->data['secure']) {
+            $baseUri = 'https://apilayer.net/api/';
+        }
+
+        $body = [
+            'query' => $this->data
+        ];
+
+        return $this
+            ->setBaseUri($baseUri)
+            ->setEndpoint('check')
+            ->setBody($body)
+            ->verify();
     }
 }
